8aeba244eb1ccb64f0a7aac13bfa71ffe8aae73f
Gerardo Loperena <1830472@upv.edu.mx>
2020-10-07 15:36:50 -0500

    v5

--- U2/Practicas/Ejercicio03/script.js
+++ U2/Practicas/Ejercicio03/script.js
@@ -2,7 +2,6 @@ function calcularIMC(){
 	 
    var imcResultado = document.getElementById("imcResultado");
    var valoracion= document.getElementById("valoracion");
-   var img=document.getElementById("img");
    
    var peso = document.getElementById("peso").value;
    var altura = document.getElementById("altura").value;
@@ -14,22 +13,17 @@ function calcularIMC(){
 	    imc = peso/(altura * altura);
 		if(imc<=18.5){
 			val="Infrapeso";
-			img.innerHTML='<img src="Imagenes/inf.png">';
 		} else {
 			if(imc>18.8&&imc<=24.9){
 				val="Peso normal";
-				img.innerHTML='<img src="Imagenes/norm.png">';
 			} else {
 				if(imc>=25&imc<=29.9){
 					val="Sobrepeso";
-					img.innerHTML='<img src="Imagenes/so.png">';
 				} else {
 					if(imc>=30&imc<=34.9){
 						val="Obesidad";
-						img.innerHTML='<img src="Imagenes/ob.png">';
 					} else {
 						val="Obesidad Morbida";
-						img.innerHTML='<img src="Imagenes/obM.png">';
 					}
 				}
 			}
@@ -42,20 +36,4 @@ function calcularIMC(){
    }
    
     
-}
-function limpiar(){
-	var imcResultado = document.getElementById("imcResultado");
-    var valoracion= document.getElementById("valoracion");
-    var img=document.getElementById("img");
-	
-	var peso = document.getElementById("peso");
-	var altura = document.getElementById("altura");
-   
-	peso.value="";
-	altura.value="";
-	
-	img.innerHTML='';
-	imcResultado.innerHTML = "";
-	valoracion.innerHTML= "";
-	
 }
